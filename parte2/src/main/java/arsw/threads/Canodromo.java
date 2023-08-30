b59ad6f19cf75b102e222c197f2ee0f006f20b9b
Andrea <andrea.duran@mail.escuelaing.edu.co>
2023-08-30 18:04:25 -0500

    Parte 2 e imágenes del README

--- parte2/src/main/java/arsw/threads/Canodromo.java
+++ parte2/src/main/java/arsw/threads/Canodromo.java
@@ -157,6 +157,6 @@ public class Canodromo extends JFrame {
 	}
 	
 	public void winnerDialog(String winner,int total) {
-            JOptionPane.showMessageDialog(null, "El ganador fue:" + winner + " de un total de " + total);
+            JOptionPane.showMessageDialog(null, "El ganador fue : " + winner + " de un total de " + total);
         }	
 }
